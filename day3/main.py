b71f635c7b6f54f0640ec71e865af1fe06bd15cd
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2022-12-10 16:50:38 -0600

    day3 part2 finished

--- day3/main.py
+++ day3/main.py
@@ -46,28 +46,73 @@ letter_scores = {
 }
 
 score = 0
+count = 0
+
+lines = []
+
+first_run = True
+
+part1 = False
 
 file = open("input.txt", "r")
-for line in file:
+if (part1 == True):
+    for line in file:
 
-    # strip the new line char. might not even be needed
-    line = line.strip("\n") 
+        # strip the new line char. might not even be needed
+        line = line.strip("\n") 
 
-    half_list = int(len(line)/2) 
+        half_list = int(len(line)/2) 
 
-    compartment1 = line[0:half_list]
-    compartment2 = line[half_list:]
+        compartment1 = line[0:half_list]
+        compartment2 = line[half_list:]
 
-    letters_done = []
+        letters_done = []
 
-    for x in range(half_list):
-        for y in range (half_list):
-            if ( (compartment1[x] == compartment2[y]) and (not(compartment1[x] in letters_done))):
-                score += int(letter_scores[compartment1[x]])
-                letters_done.append(compartment1[x])
-                print(f"full list: {line}\n comp1: {compartment1}\n comp2: {compartment2}\n matching char: {compartment2[y]}\n index: [{x},{y}]\n score: {score}\n letter_value: {letter_scores[compartment1[x]]}\n")
-                # continue
+        for x in range(half_list):
+            for y in range (half_list):
+                if ( (compartment1[x] == compartment2[y]) and (not(compartment1[x] in letters_done))):
+                    score += int(letter_scores[compartment1[x]])
+                    letters_done.append(compartment1[x])
+                    # print(f"full list: {line}\n comp1: {compartment1}\n comp2: {compartment2}\n matching char: {compartment2[y]}\n index: [{x},{y}]\n score: {score}\n letter_value: {letter_scores[compartment1[x]]}\n")
 
-            # add the point value of letter 
 
-print(score)
+    # start of part 2
+else:
+        for line in file:
+
+            # strip new line char and add it to the list
+            line = line.strip("\n") 
+            lines.append(line)
+
+            # if we have 3 lines in the list and its not the first time being ran
+            if ((len(lines) == 3) and (first_run == False)):
+                
+                # init the vars
+                letters_done = []
+                length1 = len(lines[0])
+                length2 = len(lines[1])
+                length3 = len(lines[2])
+
+                # loop every char in all posible spots 
+                for x in range(length1):
+                    for y in range(length2):
+                        for z in range(length3):
+
+                            # if all the same char exists add it to the score
+                            if ( (lines[0][x] == lines[1][y]) and (lines[0][x] == lines[2][z]) and (not(lines[0][x] in letters_done)) ):
+                                score += int(letter_scores[lines[0][x]])
+                                letters_done.append(lines[0][x])
+                                # print(f"lines[0]: {lines[0]}")
+                                # print(f"lines[1]: {lines[1]}")
+                                # print(f"lines[2]: {lines[2]}")
+                                # print(f"lines[0][x]: {lines[0][x]}")
+                                # print(f"lines[1][x]: {lines[1][y]}")
+                                # print(f"lines[2][x]: {lines[2][z]}")
+                                # print(f"score: {int(letter_scores[lines[0][x]])}")
+                                # print("\n\n")
+                                continue
+                lines = []
+            first_run = False
+
+
+print(f"score: {score}")
